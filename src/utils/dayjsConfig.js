7dd36e41eb4e68e6ef9a011c472b23c7ef3cd9b6
ngbaduyy10 <ngbaduyy05@gmail.com>
2025-03-22 13:24:49 +0700

    utc fixed

--- src/utils/dayjsConfig.js
+++ src/utils/dayjsConfig.js
@@ -5,4 +5,4 @@ import timezone from 'dayjs/plugin/timezone';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-export const dayjsDate = (date) => dayjs.utc(date).add(7, 'hour').tz('Asia/Ho_Chi_Minh').format('HH:mm DD-MM-YYYY');
+export const dayjsDate = (date) => dayjs.utc(date).tz('Asia/Ho_Chi_Minh').format('HH:mm DD-MM-YYYY');
