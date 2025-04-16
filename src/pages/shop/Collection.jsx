3339823a497517b8da0494ce42b38c57e9bdcfcb
Tyler <atunguye25@gmail.com>
2025-04-16 11:11:36 +0700

    Add filtering for ONE category

--- src/pages/shop/Collection.jsx
+++ src/pages/shop/Collection.jsx
@@ -1,19 +1,30 @@
-import { Button } from '../../components/ui/button.jsx';
-import { Checkbox } from '../../components/ui/checkbox.jsx';
-import { Label } from '../../components/ui/label.jsx';
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuRadioGroup, DropdownMenuRadioItem } from '../../components/ui/dropdown-menu.jsx';
-import {brandOptions, capitalizeFirstLetter, sortOptions} from "@/utils/index.js";
-import {ArrowUpDownIcon, RotateCcw} from "lucide-react";
-import {useEffect, useState} from "react";
+import { Button } from "../../components/ui/button.jsx";
+import { Checkbox } from "../../components/ui/checkbox.jsx";
+import { Label } from "../../components/ui/label.jsx";
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+} from "../../components/ui/dropdown-menu.jsx";
+import {
+    brandOptions,
+    capitalizeFirstLetter,
+    sortOptions,
+} from "@/utils/index.js";
+import { ArrowUpDownIcon, RotateCcw } from "lucide-react";
+import { useEffect, useState } from "react";
 import ItemCard from "@/components/ItemCard.jsx";
-import {Input} from "@/components/ui/input.jsx";
-import {getShoes} from "@/services/shoes.service.js";
-import {getCategories} from "@/services/category.service.js";
+import { Input } from "@/components/ui/input.jsx";
+import { getShoes, getShoesByCategories } from "@/services/shoes.service.js";
+import { getCategories } from "@/services/category.service.js";
 
 const Collection = () => {
     const [sort, setSort] = useState("default");
     const [itemList, setItemList] = useState([]);
     const [categoryList, setCategoryList] = useState([]);
+    const [selectedCategories, setSelectedCategories] = useState([]);
 
     useEffect(() => {
         const fetchCategories = async () => {
@@ -21,18 +32,25 @@ const Collection = () => {
             if (response.success) {
                 setCategoryList(response.data);
             }
-        }
+        };
 
         fetchCategories();
 
         const fetchShoes = async () => {
-            const response = await getShoes();
-            if (response.success) {
-                setItemList(response.data);
+            if (selectedCategories.length > 0) {
+                const response = await getShoesByCategories(selectedCategories);
+                if (response.success) {
+                    setItemList(response.data);
+                }
+            } else {
+                const response = await getShoes();
+                if (response.success) {
+                    setItemList(response.data);
+                }
             }
-        }
+        };
         fetchShoes();
-    }, []);
+    }, [selectedCategories]);
 
     return (
         <>
@@ -46,19 +64,41 @@ const Collection = () => {
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6 bg-background">
                     <div className="flex flex-col hidden md:block">
-                        <div className="text-xl font-bold mb-3 py-2">FILTERS</div>
+                        <div className="text-xl font-bold mb-3 py-2">
+                            FILTERS
+                        </div>
                         <div className="flex flex-col gap-2 border rounded-lg p-3 mb-3">
                             <div className="flex-between">
-                                <h3 className="text-sm font-semibold">CATEGORIES</h3>
-                                <Button size="sm" variant="outline"
-                                        className="h-[24px] w-[24px] cursor-pointer hover:bg-primary hover:text-white">
-                                    <RotateCcw/>
+                                <h3 className="text-sm font-semibold">
+                                    CATEGORIES
+                                </h3>
+                                <Button
+                                    size="sm"
+                                    variant="outline"
+                                    className="h-[24px] w-[24px] cursor-pointer hover:bg-primary hover:text-white"
+                                >
+                                    <RotateCcw />
                                 </Button>
                             </div>
                             <div className="flex flex-col gap-2">
                                 {categoryList.map((category) => (
-                                    <Label className="flex items-center gap-2 text-foreground" key={category.id}>
-                                        <Checkbox/>
+                                    <Label
+                                        className="flex items-center gap-2 text-foreground"
+                                        key={category.id}
+                                    >
+                                        <Checkbox
+                                            onCheckedChange={(checked) => {
+                                                setSelectedCategories((prev) =>
+                                                    checked
+                                                        ? [...prev, category.id]
+                                                        : prev.filter(
+                                                              (id) =>
+                                                                  id !==
+                                                                  category.id,
+                                                          ),
+                                                );
+                                            }}
+                                        />
                                         {capitalizeFirstLetter(category.name)}
                                     </Label>
                                 ))}
@@ -66,16 +106,24 @@ const Collection = () => {
                         </div>
                         <div className="flex flex-col gap-2 border rounded-lg p-3">
                             <div className="flex-between">
-                                <h3 className="text-sm font-semibold">BRANDS</h3>
-                                <Button size="sm" variant="outline"
-                                        className="h-[24px] w-[24px] cursor-pointer hover:bg-primary hover:text-white">
-                                    <RotateCcw/>
+                                <h3 className="text-sm font-semibold">
+                                    BRANDS
+                                </h3>
+                                <Button
+                                    size="sm"
+                                    variant="outline"
+                                    className="h-[24px] w-[24px] cursor-pointer hover:bg-primary hover:text-white"
+                                >
+                                    <RotateCcw />
                                 </Button>
                             </div>
                             <div className="flex flex-col gap-2">
                                 {brandOptions.map((option) => (
-                                    <Label className="flex items-center gap-2 text-foreground" key={option.id}>
-                                        <Checkbox/>
+                                    <Label
+                                        className="flex items-center gap-2 text-foreground"
+                                        key={option.id}
+                                    >
+                                        <Checkbox />
                                         {option.label}
                                     </Label>
                                 ))}
@@ -85,9 +133,15 @@ const Collection = () => {
 
                     <div className="flex flex-col">
                         <div className="flex items-center justify-end sm:justify-between mb-3">
-                            <div className="text-xl font-bold py-2 hidden sm:block">COLLECTIONS</div>
+                            <div className="text-xl font-bold py-2 hidden sm:block">
+                                COLLECTIONS
+                            </div>
                             <div className="flex items-center gap-2">
-                                <Input type="text" placeholder="Search" className="sm:w-[250px] lg:w-[400px]"/>
+                                <Input
+                                    type="text"
+                                    placeholder="Search"
+                                    className="sm:w-[250px] lg:w-[400px]"
+                                />
                                 <DropdownMenu>
                                     <DropdownMenuTrigger asChild>
                                         <Button
@@ -95,14 +149,19 @@ const Collection = () => {
                                             size="sm"
                                             className="flex items-center gap-1 cursor-pointer hover:bg-primary hover:text-white h-[36px]"
                                         >
-                                            <ArrowUpDownIcon className="h-4 w-4"/>
+                                            <ArrowUpDownIcon className="h-4 w-4" />
                                             <span>Sort by</span>
                                         </Button>
                                     </DropdownMenuTrigger>
-                                    <DropdownMenuContent align="end" className="w-[200px]">
+                                    <DropdownMenuContent
+                                        align="end"
+                                        className="w-[200px]"
+                                    >
                                         <DropdownMenuRadioGroup
                                             value={sort}
-                                            onValueChange={(value) => setSort(value)}
+                                            onValueChange={(value) =>
+                                                setSort(value)
+                                            }
                                         >
                                             {sortOptions.map((sortItem) => (
                                                 <DropdownMenuRadioItem
@@ -128,6 +187,7 @@ const Collection = () => {
             </div>
         </>
     );
-}
+};
 
 export default Collection;
+
--- src/services/shoes.service.js
+++ src/services/shoes.service.js
@@ -4,9 +4,16 @@ import { BASE_URL } from "../utils/index.js";
 export const getShoes = async () => {
     const response = await axios.get(BASE_URL + "/shoes");
     return response.data;
-}
+};
 
 export const getShoeById = async (shoesId) => {
     const response = await axios.get(BASE_URL + "/shoes/" + shoesId);
     return response.data;
-}
+};
+
+export const getShoesByCategories = async (categoryIds) => {
+    const response = await axios.get(
+        `${BASE_URL}/shoes/category/${categoryIds.join(",")}`,
+    );
+    return response.data;
+};
